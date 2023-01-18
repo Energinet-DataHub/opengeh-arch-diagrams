2121e7b8f857c756b4329fa563403851ca765228
Søren Søndergaard <s.sondergaard@icloud.com>
2023-01-18 08:12:14 +0100

    First commit

---
+++ source/prg/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+
+const string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+
+using var connection = new SqlConnection(connectionString);
+
+
+
+Console.WriteLine("Hello, World!");
